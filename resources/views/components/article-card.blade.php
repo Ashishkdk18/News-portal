cd5ed26ad60f7cd27b39ae2e7e3db09ba0910779
Ashish Khadka <firedemonrb@gmail.com>
2025-07-25 13:26:23 +0545

    Initial commit

---
+++ resources/views/components/article-card.blade.php
@@ -0,0 +1,18 @@
+@props(['article'])
+
+<a href="{{ route('article', $article->id) }}"
+   class="grid grid-cols-3 gap-4 shadow hover:shadow-lg rounded-md overflow-hidden">
+   <img class="h-full w-full object-cover" src="{{ asset($article->image) }}" alt="{{ $article->title }}">
+   <div class="col-span-2 p-2">
+       <h3 class="line-clamp-2 font-bold">
+           {{ $article->title }}
+       </h3>
+       <small>
+           @if ($article->created_at)
+               {{ Anuzpandey\LaravelNepaliDate\LaravelNepaliDate::from($article->created_at)->toNepaliDate(format: 'D, j F Y', locale: 'np') }}
+           @else
+               <span>मिति उपलब्ध छैन</span>
+           @endif
+       </small>
+   </div>
+</a>
